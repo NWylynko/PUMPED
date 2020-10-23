731ba40b33263a98ff1adc37bc20c635349c644d
root <root@ITE23511.ad.ccgs.wa.edu.au>
2020-10-23 22:21:51 +0800

    use glowing sign

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import PUMPEDimg from './PUMPED.png'
 
 export default function Navbar() {
   return (
     <header>
-      <Link to="/" style={{ textDecoration: 'none' }}><PUMPED>PUMPED</PUMPED></Link>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <PUMPEDContainer>
+          <PUMPED src={PUMPEDimg} />
+        </PUMPEDContainer>
+      </Link>
 
     </header>
   )
 }
 
-const PUMPED = styled.h1`
-  font-family: sporty-pro-inline, sans-serif;
-  font-weight: 900;
-  font-style: normal;
-
-  font-size: 4em;
-
-  background-color: #ff0000;
-  color: #fff;
-
-  margin: 20px;
-  padding: 20px;
+const PUMPED = styled.img`
+  max-height: 300px;
 `;
 
+const PUMPEDContainer = styled.div`
+
+justify-content: center;
+align-items: center;
+display: inline-flex;
+
+  animation: slideup 2s ease-in-out;
+
+  @keyframes slideup {
+    from {
+      height: 100vh;
+    }
+
+    to {
+      height: 300px;
+    }
+  }
+`;
