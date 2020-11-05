46a1b20920931dd35c49e99cd9fd7cb4e9cd76c9
Nick Wylynko <nick@BigMac.local>
2020-11-05 23:47:30 +0800

    fix cart issues, calc total and add buttons

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -1,12 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import StoreContext from "../../store";
 import styled from "styled-components";
 import { useQuery } from "react-query";
 import { getCart, getShoe } from "../../api";
 import type { OrderItem } from "PUMPED-api/src/api/order/types";
 import { apiEndpoint } from "../../config";
-import { Redirect } from "react-router-dom";
 import { Loading } from "../../components/loading";
+import { Link } from "react-router-dom";
 
 export const CheckCustomerID = () => {
   const { customer } = useContext(StoreContext);
@@ -23,14 +23,26 @@ export const CheckCustomerID = () => {
   return <Cart />;
 };
 
-interface Props {
-}
+interface Props {}
 
 export const Cart = (props: Props): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState<OrderItem[]>();
 
-  const { isLoading, error, data } = useQuery(`cart`, () =>
-    getCart()
-  );
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const cart = await getCart();
+        setData(cart);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    run();
+  }, []);
 
   if (isLoading) {
     return <Loading />;
@@ -49,10 +61,22 @@ export const Cart = (props: Props): JSX.Element => {
     );
   }
 
+  const total = data.reduce(
+    (sum, { currentPrice, quantity }) => sum + currentPrice * quantity,
+    0
+  );
+
   return (
     <Page>
       <h2>Cart</h2>
       <Shoes data={data} />
+      <TotalContainer>
+        <Total>Total ${total}</Total>
+      </TotalContainer>
+      <ButtonContainer>
+        <button>Clear the cart</button>
+        <button>Checkout</button>
+      </ButtonContainer>
     </Page>
   );
 };
@@ -64,38 +88,54 @@ const Page = styled.div`
   align-items: center;
 `;
 
-const Shoes = ({ data }: {data: OrderItem[] }) => {
-  return ( <>{data.map(Shoe)}</> )
-}
+const Total = styled.p`
+  width: 700px;
+  text-align: end;
+  margin: 2px;
+  padding: 10px;
+  border-radius: 5px;
+
+  background-color: #131212;
+`;
+
+const TotalContainer = styled.div`
+  margin: 10px;
+  background: linear-gradient(to right, #131212, #131212, #131212, white);
+  border-radius: 5px;
+`;
+
+const ButtonContainer = styled.div`
+  /* align-items: center; */
+`;
+
+const Shoes = ({ data }: { data: OrderItem[] }) => {
+  return <>{data.map(Shoe)}</>;
+};
 
 const Shoe = ({ ShoeID, StockID, quantity }: OrderItem) => {
-  const [redirect, setRedirect] = useState('');
-
   const { isLoading, error, data } = useQuery(`shoe ${ShoeID}`, () =>
     getShoe(ShoeID)
   );
 
   if (isLoading) {
-    return <Loading />;
+    return <Loading key={ShoeID} />;
   }
 
   if (error) {
-    return <p>error</p>;
+    return <p key={ShoeID}>error</p>;
   }
 
   if (!data) {
-    return <p>shoe doesnt exist</p>;
-  }
-
-  if (redirect) {
-    return <Redirect to={redirect}/>
+    return <p key={ShoeID}>shoe doesnt exist</p>;
   }
 
   return (
-    <Container onClick={() => setRedirect(`/shoe/${ShoeID}`)}>
+    <Container key={ShoeID}>
       <SmallImage src={`${apiEndpoint}image/${data.CoverImage}/low`} />
-      <p>{data.Name}</p>
-      <p>quantity: {quantity}</p>
+      <NameLink to={`/shoe/${ShoeID}`}>
+        <Name>{data.Name}</Name>
+      </NameLink>
+      <p>Quantity {quantity}</p>
       <p>${data.Price}</p>
     </Container>
   );
@@ -103,8 +143,10 @@ const Shoe = ({ ShoeID, StockID, quantity }: OrderItem) => {
 
 const Container = styled.div`
   display: inline-flex;
-  width: 500px;
+  width: 700px;
   justify-content: space-between;
+  text-decoration: none;
+  align-items: center;
 `;
 
 const SmallImage = styled.img`
@@ -115,4 +157,16 @@ const SmallImage = styled.img`
 
   padding: 2px;
   margin: 2px;
+`;
+
+const Name = styled.h3`
+  min-width: 400px;
+`;
+
+const NameLink = styled(Link)`
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
