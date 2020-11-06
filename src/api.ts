beb79a4bdb1481ec5a53a68244bcd9d07d7d12c7
root <root@ITE23511.ad.ccgs.wa.edu.au>
2020-11-06 14:57:57 +0800

    add collection and colour endpoint functions

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import { apiEndpoint } from './config'
 import type { BrandWithID, Brand, partOfBrand, partOfBrandWithID } from 'PUMPED-api/src/api/brand/types';
-import type { removedCartItem, clearedCart } from 'PUMPED-api/src/api/cart/types';
+import type { removedCartItem, clearedCart, AddedToCart } from 'PUMPED-api/src/api/cart/types';
+import type { Collection, CollectionWithID } from 'PUMPED-api/src/api/collection/types';
+import type { Colour, ColourWithID, partOfColour, updatedColour, addedColour } from 'PUMPED-api/src/api/colour/types';
 import type { ShoeWithColours, ShoeWithDetails } from 'PUMPED-api/src/api/shoe/types';
 import type { Review } from 'PUMPED-api/src/api/review/types';
 import type { Customer, CustomerWithID } from 'PUMPED-api/src/api/customer/types';
@@ -68,25 +70,32 @@ const x = {
 }
 
 // brand
-export const getBrand = (BrandID: number | string): Promise<BrandWithID> => x.get(`brand/${BrandID}`)
-export const addBrand = (fields: Brand): Promise<Brand> => x.post(`brand`, fields)
-export const updateBrand = (BrandID: number | string, fields: partOfBrand): Promise<partOfBrandWithID> => x.patch(`brand/${BrandID}`, fields)
-export const removeBrand = (BrandID: number | string): Promise<{BrandID: number}> => x.delete(`brand/${BrandID}`)
+export const getBrand =         (BrandID: number | string):                           Promise<BrandWithID>                                          => x.get(`brand/${BrandID}`)
+export const addBrand =         (fields: Brand):                                      Promise<Brand>                                                => x.post(`brand`, fields)
+export const updateBrand =      (BrandID: number | string, fields: partOfBrand):      Promise<partOfBrandWithID>                                    => x.patch(`brand/${BrandID}`, fields)
+export const removeBrand =      (BrandID: number | string):                           Promise<{BrandID: number}>                                    => x.delete(`brand/${BrandID}`)
 
 //cart
-export const getCart = (): Promise<OrderItem[]> => x.get(`/cart`)
-export const addToCart = (ShoeID: number | string, fields: partOfOrderItem) => x.post(`cart/add/${ShoeID}`, fields)
-export const checkoutCart = (address: string): Promise<{ CustomerID: number; OrderID: number; }> => x.post(`cart/checkout`, { address })
-export const updateCart = (ShoeID: number | string, fields: partOfOrderItem): Promise<partOfOrderItemWithIDs> => x.patch(`cart/${ShoeID}`, fields)
-export const removeCartItem = (ShoeID: number | string): Promise<removedCartItem> => x.delete(`cart/${ShoeID}`)
-export const clearCart = (): Promise<clearedCart> => x.delete(`cart`)
+export const getCart =          ():                                                   Promise<OrderItem[]>                                          => x.get(`cart`)
+export const addToCart =        (ShoeID: number | string, fields: partOfOrderItem):   Promise<AddedToCart>                                          => x.post(`cart/add/${ShoeID}`, fields)
+export const checkoutCart =     (address: string):                                    Promise<{ CustomerID: number; OrderID: number; }>             => x.post(`cart/checkout`, { address })
+export const updateCart =       (ShoeID: number | string, fields: partOfOrderItem):   Promise<partOfOrderItemWithIDs>                               => x.patch(`cart/${ShoeID}`, fields)
+export const removeCartItem =   (ShoeID: number | string):                            Promise<removedCartItem>                                      => x.delete(`cart/${ShoeID}`)
+export const clearCart =        ():                                                   Promise<clearedCart>                                          => x.delete(`cart`)
 
-export const getShoes = (): Promise<ShoeWithColours[]> => x.get('shoe')
+//collection
+export const getCollection =    (CollectionID: number | string):                      Promise<CollectionWithID>                                     => x.get(`collection/${CollectionID}`)
+export const addCollection =    (fields: Collection):                                 Promise<{ name: string; }>                                    => x.post(`collection`, fields)
+export const updateCollection = (CollectionID: number | string, fields: Collection):  Promise<{ name?: string | undefined; CollectionID: number; }> => x.patch(`collection/${CollectionID}`, fields)
+export const removeCollection = (CollectionID: number | string):                      Promise<{ CollectionID: number; }>                            => x.delete(`collection/${CollectionID}`)
 
-export const getShoe = (id: string | number): Promise<ShoeWithDetails> => x.get(`shoe/${id}`)
+//colour
+export const getColour =        (ColourID: number | string):                          Promise<ColourWithID>                                         => x.get(`colour/${ColourID}`)
+export const addColour =        (fields: Colour):                                     Promise<addedColour>                                          => x.post(`colour`, fields)
+export const updateColour =     (ColourID: number | string, fields: partOfColour):    Promise<updatedColour>                                        => x.patch(`colour/${ColourID}`, fields)
+export const removeColour =     (ColourID: number | string):                          Promise<{ ColourID: number; }>                                => x.delete(`colour/${ColourID}`)
 
-export const getReviews = (id: string | number): Promise<Review[]> => x.get(`review/${id}`)
-
+//customer
 export const addCustomer = async (fields: Customer): Promise<CustomerWithID> => { 
   const CustomerDetails = await x.post(`customer`, fields) 
 
@@ -95,3 +104,23 @@ export const addCustomer = async (fields: Customer): Promise<CustomerWithID> => 
   return CustomerDetails
 }
 
+//image
+
+//order
+
+//review
+export const getReviews = (id: string | number): Promise<Review[]> => x.get(`review/${id}`)
+
+//section
+
+//shoe
+export const getShoes = (): Promise<ShoeWithColours[]> => x.get('shoe')
+export const getShoe = (id: string | number): Promise<ShoeWithDetails> => x.get(`shoe/${id}`)
+
+//stock
+
+//style
+
+//tag
+
+//wishlist
