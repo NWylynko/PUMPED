8bae78daa933152e7b181c1a0304b7eac8a0f105
Nick Wylynko <nick@BigMac.local>
2020-11-06 21:58:56 +0800

    wishlist button and custom button to add to cart

--- src/pages/shoe/ShoePage.tsx
+++ src/pages/shoe/ShoePage.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
-import { addToCart, getShoe } from "../../api";
+import { addToCart, getShoe, addToWishlist, removeFromWishlist, isInWishlist } from "../../api";
 import type { ShoeWithDetails as ShoeProps } from "PUMPED-api/src/api/shoe/types";
 import { apiEndpoint } from "../../config";
 import styled from "styled-components";
 import { Stars } from "../../components/Stars";
 import { Images } from "./Images";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
-import Reviews from './reviews'
-import StoreContext from '../../store'
+import Reviews from "./reviews";
+import StoreContext from "../../store";
 import { Loading } from "../../components/loading";
+import { TiTickOutline, TiTimesOutline } from "react-icons/ti";
 
 export function ShoePage() {
   let { id }: { id: string } = useParams();
@@ -50,9 +51,8 @@ function Shoe({
   Brand,
   BrandIcon,
   colours,
-  stock
+  stock,
 }: ShoeProps) {
-
   const { customer } = useContext(StoreContext);
 
   return (
@@ -74,25 +74,106 @@ function Shoe({
           <Text>Style: {Style}</Text>
           <Text>Section: {Section}</Text>
           <Text>Collection: {Collection}</Text>
-          <button onClick={() => {
-            if (customer) {
-              addToCart(ID, { StockID: stock[0].ID, quantity: 1 })
-            }
-          }}>Add to Cart</button>
-          <button>
-            Add to Wishlist{" "}
-            {false ? (
-              <AiFillStar color="white" />
-            ) : (
-              <AiOutlineStar color="white" />
-            )}
-          </button>
+          <Horizontal style={{ width: "100%" }}>
+            <WishlistButton ShoeID={ID} />
+            <Button
+              onClick={() => {
+                if (customer) {
+                  return addToCart(ID, { StockID: stock[0].ID, quantity: 1 });
+                }
+              }}
+              style={{ width: "100%" }}
+            >
+              <h3>Add to Cart</h3>
+            </Button>
+          </Horizontal>
         </BodyText>
       </Body>
       <Reviews shoeID={ID} />
     </Page>
   );
 }
+
+interface WishlistButtonProps {
+  ShoeID: number;
+}
+
+const WishlistButton = ({ ShoeID }: WishlistButtonProps) => {
+
+  const [fav, setFav] = useState(false)
+
+  useEffect(() => {
+    const run = async () => {
+      const isFav = await isInWishlist(ShoeID)
+      setFav(isFav)
+    }
+    run()
+  }, [ShoeID])
+
+  return (
+    <button style={{ minWidth: 75 }} onClick={() => {
+      if (!fav) {
+        addToWishlist(ShoeID)
+        setFav(true)
+      } else {
+        removeFromWishlist(ShoeID)
+        setFav(false)
+      }
+    }}>
+      {fav ? (
+        <AiFillStar color="white" size={32} />
+      ) : (
+        <AiOutlineStar color="white" size={32} />
+      )}
+    </button>
+  );
+};
+
+interface ButtonProps {
+  children: string | JSX.Element;
+  onClick?: () => Promise<any> | any;
+  Success?: JSX.Element;
+  Fail?: JSX.Element;
+  style?: React.CSSProperties;
+}
+
+const Button = ({ children, onClick, Success, Fail, style }: ButtonProps) => {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failed, setFailed] = useState(false);
+  const [error, setError] = useState();
+
+  const clickHandler = async () => {
+    try {
+      setSuccess(false);
+      setFailed(false);
+      setLoading(true);
+      if (onClick) {
+        await onClick();
+      }
+      setSuccess(true);
+    } catch (error) {
+      setError(error);
+      setFailed(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <button onClick={clickHandler} style={style}>
+      {loading ? (
+        <Loading />
+      ) : success ? (
+        Success || <TiTickOutline size={28} />
+      ) : failed ? (
+        Fail || <TiTimesOutline size={28} />
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
 
 const Page = styled.div`
   min-height: calc(101vh - 300px);
@@ -138,6 +219,9 @@ export const StyledPriceText = styled.h3`
   padding: 10px;
 
   font-family: "Anton", sans-serif;
+
+  align-items: center;
+  display: inline-flex;
 `;
 
 const BodyText = styled.div`
